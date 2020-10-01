95fb1375385aac7b8e1e816cdf8c286dadf40cb3
Engels May <inhfrukiwi@yandex.ru>
2020-10-01 10:45:41 +0300

    Парсим отельчики

---
+++ src/spiders/tourism_classification.py
@@ -0,0 +1,73 @@
+import time
+import scrapy
+
+from urllib.parse import parse_qs, urlparse
+
+
+# TODO: миддлвар на toomanyrequest
+# https://stackoverflow.com/questions/43630434/how-to-handle-a-429-too-many-requests-response-in-scrapy
+
+
+class TourismClassificationHotelSpider(scrapy.Spider):
+    name = 'tourism_classification_hotel'
+    # start_requests = [
+    #     'https://xn----7sba3acabbldhv3chawrl5bzn.xn--p1ai/displayAccommodation'
+    #     '/index?Accommodation_page=1'
+    # ]
+
+    def start_requests(self):
+        urls = [
+            'https://xn----7sba3acabbldhv3chawrl5bzn.xn--p1ai/displayAccommodation/index?Accommodation_page=1'
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        hotel_links_xpath = (
+            "//div[@id='yw1']/div[@class='items']/div[@class='object']/"
+            "div[@class='object-left']/a/@href"
+        )
+        print('\nПарсинг отелей страницы [%s]' % response.url)
+        yield from response.follow_all(
+            response.xpath(hotel_links_xpath), callback=self.hotel_page_parse
+        )
+
+        current_page = self.get_current_page(response.url)
+        links = {
+            int(link_el.xpath('text()').get(default=0)): link_el.xpath('@href').get(default='') # NOQA
+            for link_el in response.xpath(
+                "//div[@id='yw1']/div[@class='pager']/"
+                "ul[@class='pagination']/li[@class='page']/a"
+            )
+        }
+        nearest_links = [
+            t[1] for t in filter(
+                lambda t, current_page=current_page: t[0] > current_page,
+                links.items()
+            )
+        ]
+        if nearest_links:
+            yield response.follow(nearest_links[0], callback=self.parse)
+
+    def hotel_page_parse(self, response):
+        detail_fields = response.xpath(
+            "//div[@class='content']/div[@class='content_left']/"
+            "div[@class='detail-fields']/div[@class='detail-field']"
+        )
+        obj = {
+            'url': response.url,
+        }
+        for detail_field in detail_fields:
+            label = detail_field.css('.detail-label::text').get()
+            value = detail_field.css('.detail-value::text').get()
+            obj[label] = value
+
+        if len(obj.keys()) > 1:
+            obj['status'] = 'Parsed'
+        else:
+            obj['status'] = 'Error'
+
+        yield obj
+
+    def get_current_page(self, url):
+        return int(parse_qs(urlparse(url).query)['Accommodation_page'][0])
